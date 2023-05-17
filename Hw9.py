a8d4b17ee5eb2bd95c5e7068bcb8b8d5fe020ddc
Anzhela7657 <lomachevskaa@gmail.com>
2023-05-17 17:08:31 +0300

    homework 9

---
+++ Hw9.py
@@ -0,0 +1,12 @@
+#Write a program that simply outputs which cats have hats at the end.
+
+cats = []
+number = int(input('Enter the number of cat: '))
+for i in range(1, number+1):
+    for j in range(1, number+1):
+        if j % i == 0:
+            if j in cats:
+                cats.remove(j)
+            else:
+                cats.append(j)
+print('The cats with hats are: ', cats)
